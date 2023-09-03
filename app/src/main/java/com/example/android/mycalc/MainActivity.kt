efc02bb56003a8a5c0edf683430abc0d19275ee5
Tanish Goyal <goyaltanish789@gmail.com>
2023-09-03 23:00:02 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/mycalc/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.android.mycalc
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.android.mycalc.ui.theme.ComposeCalculatorTheme
+import com.example.android.mycalc.ui.theme.MediumGray
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeCalculatorTheme {
+                val viewModel = viewModel<CalculatorViewModel>()
+                val state = viewModel.state
+                val buttonSpacing = 8.dp
+                Calculator(
+                    state = state,
+                    onAction = viewModel::onAction,
+                    buttonSpacing = buttonSpacing,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MediumGray)
+                        .padding(16.dp)
+                )
+            }
+        }
+    }
+}
